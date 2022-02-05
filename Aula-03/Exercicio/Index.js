ae5872dd3737f77a73a1f7b859bffb39ce6305b4
Jean Barbosa <jeanbarbosa.it@gmail.com>
2022-02-05 11:27:25 -0300

    Interação com a Aula-03 e exercicio.

---
+++ Aula-03/Exercicio/Index.js
@@ -0,0 +1,2 @@
+/*Exercicios com o console.log*/
+console.log(`Meu nome é "Nome". Estou aprendendo JavaScript as`,10, "da manhã.");
---
+++ Aula-03/Index.js
@@ -0,0 +1,6 @@
+/*Teste com console.log();*/
+console.log('Helo Wolrd');
+console.log("Helo Wolrd");
+console.log(`Helo Wolrd`);
+console.log(01234,4321000,12345689126);
+console.log("Teste ",15.5);
