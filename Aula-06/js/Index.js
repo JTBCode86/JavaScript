ac4666740441b425f86ccb75c9370c77aa999301
Jean Barbosa <jeanbarbosa.it@gmail.com>
2022-02-05 12:42:55 -0300

    Regras para criação de variaveis - JavaScript

---
+++ Aula-06/js/Index.js
@@ -0,0 +1,10 @@
+//Usando variáveis
+let nome = 'João';
+
+//Modo mais antigo.
+//var nome2= "João"; 
+
+console.log(nome," nasceu em 1984");
+console.log("Em 2000 ",nome," conheceu Maria.");
+console.log("Maria teve 1 filho com", nome," em 2015.");
+console.log("O filho de ", nome ,"chama-se Eduardo.");
---
+++ Aula-06/js/Script.js
@@ -0,0 +1,16 @@
+let nome;
+nome = "João";
+console.log(nome);
+/*
+    **IMPORTANTE**
+    
+    * Não é possível criar variáveis com nomes reservados.
+    * Variáveis precisam ter nomes significativos.
+    * Não é permitido começar nomes de variáveis com números.
+    * Não podem conter espaços ou traços.
+    * Para nomes compostos usar o camelCase. Ex: nomeCompleto.
+    * Variáveis são Case-sensitive.
+    * Não é permitido redeclarar variaveis com Let.
+    * Não utilize var, utilize Let.
+    
+*/
