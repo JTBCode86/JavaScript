0a486ddcb19f9d9ce3e872d80c4853dd4d70d853
Jean Barbosa <jeanbarbosa.it@gmail.com>
2022-02-26 22:13:44 -0300

    Aula 11 com exercicios e ajustes da aula 09.

---
+++ Aula-11/Exercicios/js/script.js
@@ -0,0 +1,12 @@
+let num1 = prompt('Digite um número qualquer');
+let num2 = prompt('Digite outro número qualquer.');
+
+//Convertendo
+num1= Number(num1);
+num2= Number(num2);
+
+const resultado = num1+num2; 
+alert(`O resultado é: ${resultado}`);
+
+//É possivel fazer também dessa forma:
+//alert(`O resultado é: ${num1+num2}`);
